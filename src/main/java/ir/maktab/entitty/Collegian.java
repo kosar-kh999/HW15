16750fa085b67ca48d8746c41e2b894303b537f6
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-11-28 11:42:55 +0330

    define enum for grade

---
+++ src/main/java/ir/maktab/entitty/Collegian.java
@@ -0,0 +1,24 @@
+package ir.maktab.entitty;
+
+import ir.maktab.entitty.enums.UniversityType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Collegian {
+    @Column(nullable = false, unique = true)
+    String studentNumber;
+    String universityName;
+    @Enumerated(EnumType.STRING)
+    UniversityType universityType;
+    @Temporal(TemporalType.DATE)
+    Date enteringDate;
+
+}
