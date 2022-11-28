b9bea7a1b18c7847dfeb3277a459bba226402d0b
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-11-28 23:41:23 +0330

    define UniversityInfo class

---
+++ src/main/java/ir/maktab/entitty/UniversityInfo.java
@@ -0,0 +1,29 @@
+package ir.maktab.entitty;
+
+import ir.maktab.entitty.enums.Grade;
+import ir.maktab.entitty.enums.UniversityType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class UniversityInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    String studentNumber;
+    String universityName;
+    @Enumerated(EnumType.STRING)
+    UniversityType universityType;
+    @Temporal(TemporalType.DATE)
+    Date enteringDate;
+    @Enumerated(EnumType.STRING)
+    Grade grade;
+}
