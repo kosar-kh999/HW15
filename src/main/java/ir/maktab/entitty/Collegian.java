06781267ab4868ca7e90a836bcf7ba11f700ed40
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-08 18:46:44 +0330

    define Collegian class

--- src/main/java/ir/maktab/entitty/Collegian.java
+++ src/main/java/ir/maktab/entitty/Collegian.java
@@ -23,12 +23,13 @@ public class Collegian extends Person implements Serializable {
     UniversityInfo universityInfo;
     @Enumerated(EnumType.STRING)
     MaritalStatus maritalStatus;
-    boolean dormitoryResident;
+    boolean isDormitoryResident;
     @OneToMany
     List<CreditCard> creditCards = new ArrayList<>();
 
     public Collegian(String firstName, String lastName, String fatherName, String motherName,
-                     String birthCertificateNumber, String nationalCode, Date birthDate, Account account, UniversityInfo universityInfo) {
+                     String birthCertificateNumber, String nationalCode, Date birthDate, Account account,
+                     UniversityInfo universityInfo) {
         super(firstName, lastName, fatherName, motherName, birthCertificateNumber, nationalCode, birthDate);
         this.account = account;
         this.universityInfo = universityInfo;
