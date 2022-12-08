070769333ea4badee4559b532b9dbb083fe257d1
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-08 18:53:43 +0330

    define HousingLoanService interface

--- src/main/java/ir/maktab/service/HousingLoanService.java
+++ src/main/java/ir/maktab/service/HousingLoanService.java
@@ -1,9 +1,10 @@
 package ir.maktab.service;
 
 import ir.maktab.entitty.HousingLoan;
+import ir.maktab.exception.HousingLoanException;
 
 public interface HousingLoanService {
-    void saveNewHousingLoan(HousingLoan housingLoan);
+    void saveNewHousingLoan(HousingLoan housingLoan) throws HousingLoanException;
 
     HousingLoan findById(int id);
 
