52a7ae8bb8376c1e909173276a6f38640cd8f71c
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-10 02:06:03 +0330

    define EducationalLoanServiceImpl class

--- src/main/java/ir/maktab/service/EducationalLoanService.java
+++ src/main/java/ir/maktab/service/EducationalLoanService.java
@@ -1,13 +1,16 @@
 package ir.maktab.service;
 
 import ir.maktab.entitty.EducationLoan;
+import ir.maktab.exception.LoanException;
 
 public interface EducationalLoanService {
-    void saveNewEducationalLoan(EducationLoan educationLoan);
+    void saveNewEducationalLoan(EducationLoan educationLoan) throws LoanException;
 
     EducationLoan findEducationalLoanById(int id);
 
     void updateEducationalLoan(EducationLoan educationLoan);
 
     void deleteEducationalLoan(EducationLoan educationLoan);
+
+    void getDate(EducationLoan educationLoan) throws LoanException;
 }
