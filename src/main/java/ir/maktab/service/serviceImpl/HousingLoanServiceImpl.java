0b2e3c3b056911402f62605359ffe68e3cc3d25d
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-10 02:08:14 +0330

    define HousingLoanServiceImpl class

--- src/main/java/ir/maktab/service/serviceImpl/HousingLoanServiceImpl.java
+++ src/main/java/ir/maktab/service/serviceImpl/HousingLoanServiceImpl.java
@@ -3,6 +3,8 @@ package ir.maktab.service.serviceImpl;
 import ir.maktab.entitty.Collegian;
 import ir.maktab.entitty.HousingLoan;
 import ir.maktab.entitty.enums.CityType;
+import ir.maktab.entitty.enums.MaritalStatus;
+import ir.maktab.exception.LoanException;
 import ir.maktab.repository.HousingLoanRepository;
 import ir.maktab.service.HousingLoanService;
 
@@ -11,15 +13,18 @@ public class HousingLoanServiceImpl implements HousingLoanService {
     Collegian collegian = new Collegian();
 
     @Override
-    public void saveNewHousingLoan(HousingLoan housingLoan) {
-        if (housingLoan.getCityType().equals(CityType.TEHRAN)) {
-            housingLoan.setAmount(32000000);
-        } else if (housingLoan.getCityType().equals(CityType.BIG_CITY)) {
-            housingLoan.setAmount(26000000);
-        } else if (housingLoan.getCityType().equals(CityType.OTHER_CITY)) {
-            housingLoan.setAmount(19500000);
+    public void saveNewHousingLoan(HousingLoan housingLoan) throws LoanException {
+        if (!collegian.isDormitoryResident() && collegian.getMaritalStatus().equals(MaritalStatus.MARRIED)) {
+            if (housingLoan.getCityType().equals(CityType.TEHRAN)) {
+                housingLoan.setAmount(32000000);
+            } else if (housingLoan.getCityType().equals(CityType.BIG_CITY)) {
+                housingLoan.setAmount(26000000);
+            } else if (housingLoan.getCityType().equals(CityType.OTHER_CITY)) {
+                housingLoan.setAmount(19500000);
+            }
+            housingLoanRepository.creat(housingLoan);
         }
-        housingLoanRepository.creat(housingLoan);
+        throw new LoanException(" You don't have enough requirements ! ");
     }
 
     @Override
