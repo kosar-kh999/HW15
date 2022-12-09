a794238de3241f38490d9c42c2c5a44ddb3d4c07
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-10 02:05:21 +0330

    define AllocatedLoanServiceImpl class

--- src/main/java/ir/maktab/service/serviceImpl/AllocatedLoanServiceImpl.java
+++ src/main/java/ir/maktab/service/serviceImpl/AllocatedLoanServiceImpl.java
@@ -2,22 +2,39 @@ package ir.maktab.service.serviceImpl;
 
 import ir.maktab.entitty.AllocatedLoan;
 import ir.maktab.entitty.Collegian;
+import ir.maktab.entitty.CreditCard;
 import ir.maktab.entitty.Loan;
+import ir.maktab.entitty.enums.BankType;
+import ir.maktab.exception.LoanException;
 import ir.maktab.repository.AllocatedLoanRepository;
 import ir.maktab.service.AllocatedLoanService;
+
+import java.util.Date;
 
 public class AllocatedLoanServiceImpl implements AllocatedLoanService {
     AllocatedLoanRepository allocatedLoanRepository = new AllocatedLoanRepository();
 
     @Override
-    public void saveNewAllocatedLoan(AllocatedLoan allocatedLoan) {
+    public void saveNewAllocatedLoan(Loan loan, Collegian collegian) {
+        AllocatedLoan allocatedLoan = new AllocatedLoan();
+        allocatedLoan.setLoan(loan);
+        allocatedLoan.setCollegian(collegian);
+        //allocatedLoan.setRequestDate(new Date());
         allocatedLoanRepository.persist(allocatedLoan);
     }
 
     @Override
-    public void updateAllocatedLoan(AllocatedLoan allocatedLoan) {
-        allocatedLoanRepository.update(allocatedLoan);
+    public void setCreditCard(CreditCard creditCard, AllocatedLoan allocatedLoan) throws LoanException {
+        if (creditCard.getBankType().equals(BankType.MASKAN) || creditCard.getBankType().equals(BankType.MELI)
+                || creditCard.getBankType().equals(BankType.REFAH) ||
+                creditCard.getBankType().equals(BankType.TEJARAT)) {
+            allocatedLoan.setCreditCard(creditCard);
+            allocatedLoanRepository.update(allocatedLoan);
+        } else if (creditCard.getBankType().equals(BankType.OTHER_BANK)) {
+            throw new LoanException("You don't have enough requirements !");
+        }
     }
+
 
     @Override
     public AllocatedLoan find(Loan loan, Collegian collegian) {
