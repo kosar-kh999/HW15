a9d7806046e7229aa23d5533b35d45635ea84fb3
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-10 02:03:56 +0330

    define LoanException class

--- src/main/java/ir/maktab/exception/HousingLoanException.java
+++
@@ -1,7 +0,0 @@
-package ir.maktab.exception;
-
-public class HousingLoanException extends Exception {
-    public HousingLoanException(String errorMessage) {
-        super(errorMessage);
-    }
-}
---
+++ src/main/java/ir/maktab/exception/LoanException.java
@@ -0,0 +1,7 @@
+package ir.maktab.exception;
+
+public class LoanException extends Exception {
+    public LoanException(String errorMessage) {
+        super(errorMessage);
+    }
+}
