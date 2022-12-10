f8112b8b2bc849df5721102ac15f33e7cfbdbe97
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-10 12:02:26 +0330

    define RefundService interface

---
+++ src/main/java/ir/maktab/service/RefundService.java
@@ -0,0 +1,15 @@
+package ir.maktab.service;
+
+import ir.maktab.entitty.Refund;
+
+import java.util.List;
+
+public interface RefundService {
+    void saveNewRefund(Refund refund);
+    Refund findById(int id);
+    void update(Refund refund);
+    void delete(Refund refund);
+    void increaseMonthOfPaid(Refund refund);
+    void decreaseMonthOfRemain(Refund refund);
+    List<Refund> getAll();
+}
