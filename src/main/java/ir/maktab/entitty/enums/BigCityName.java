96445ae23249c9699992d0cea8b0a6624b781c0b
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-12 03:29:58 +0330

    define BigCityName interface

--- src/main/java/ir/maktab/entitty/enums/BigCityName.java
+++ src/main/java/ir/maktab/entitty/enums/BigCityName.java
@@ -4,11 +4,11 @@ public enum BigCityName {
     GILAN, ESFAHAN, AZARBAIJAN_SHARGHI,
     FARS, KHOZESTAN, QOM, KHORASAN_RAZAVI, ALBORZ;
 
-    public CityType toCityType(){
-        switch (this){
+    public CityType toCityType() {
+        switch (this) {
             case GILAN, ESFAHAN, AZARBAIJAN_SHARGHI,
                     FARS, KHOZESTAN, QOM, KHORASAN_RAZAVI, ALBORZ:
-            return CityType.BIG_CITY;
+                return CityType.BIG_CITY;
             default:
                 return null;
         }
