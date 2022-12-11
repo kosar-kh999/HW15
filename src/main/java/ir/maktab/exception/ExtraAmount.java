aa20fa93071ba42fae9585fb6afe83b116c2369f
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-11 16:48:46 +0330

    define ExtraAmountException class

--- src/main/java/ir/maktab/exception/ExtraAmount.java
+++
@@ -1,7 +0,0 @@
-package ir.maktab.exception;
-
-public class ExtraAmount extends Exception {
-    public ExtraAmount(String errorMessage) {
-        super(errorMessage);
-    }
-}
---
+++ src/main/java/ir/maktab/exception/ExtraAmountException.java
@@ -0,0 +1,7 @@
+package ir.maktab.exception;
+
+public class ExtraAmountException extends Exception {
+    public ExtraAmountException(String errorMessage) {
+        super(errorMessage);
+    }
+}
