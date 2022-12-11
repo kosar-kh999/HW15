aa1934159de16fa6df3ee12c0aaa054cb71d68aa
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-11 17:05:32 +0330

    define StudentTuitionLoanServiceImpl class

--- src/main/java/ir/maktab/service/serviceImpl/StudentTuitionLoanServiceImpl.java
+++ src/main/java/ir/maktab/service/serviceImpl/StudentTuitionLoanServiceImpl.java
@@ -2,8 +2,12 @@ package ir.maktab.service.serviceImpl;
 
 import ir.maktab.entitty.StudentTuitionLoan;
 import ir.maktab.entitty.enums.Grade;
+import ir.maktab.exception.LoanException;
 import ir.maktab.repository.StudentTuitionLoanRepository;
 import ir.maktab.service.StudentTuitionLoanService;
+import ir.maktab.util.DateUtil;
+
+import java.time.LocalDateTime;
 
 public class StudentTuitionLoanServiceImpl implements StudentTuitionLoanService {
     StudentTuitionLoanRepository studentTuitionLoanRepository = new StudentTuitionLoanRepository();
@@ -38,5 +42,19 @@ public class StudentTuitionLoanServiceImpl implements StudentTuitionLoanService 
     @Override
     public void deleteStudentTuitionLoan(StudentTuitionLoan studentTuitionLoan) {
         studentTuitionLoanRepository.remove(studentTuitionLoan);
+    }
+
+    @Override
+    public void getDate(StudentTuitionLoan studentTuitionLoan) throws LoanException {
+        LocalDateTime firstDate = DateUtil.changeDateToLocalDateTime(studentTuitionLoan.getRequestDate());
+        LocalDateTime secondDate = DateUtil.changeDateToLocalDateTime(studentTuitionLoan.getRequestDate());
+        if (firstDate.getDayOfMonth() > 22 && firstDate.getDayOfMonth() < 30 && firstDate.getMonthValue() == 10) {
+            saveNewStudentTuitionLoan(studentTuitionLoan);
+        } else {
+            if (secondDate.getDayOfMonth() > 13 && secondDate.getDayOfMonth() < 21 && secondDate.getMonthValue() == 2) {
+                saveNewStudentTuitionLoan(studentTuitionLoan);
+            }
+        }
+        throw new LoanException("You can't request for loan in this time ! ");
     }
 }
