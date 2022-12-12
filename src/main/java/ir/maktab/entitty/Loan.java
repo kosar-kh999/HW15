1abc8171c91882cd2f62ed8c1b6062d6be1d3c4e
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-12 03:31:55 +0330

    define loan class

--- src/main/java/ir/maktab/entitty/Loan.java
+++ src/main/java/ir/maktab/entitty/Loan.java
@@ -1,6 +1,7 @@
 package ir.maktab.entitty;
 
 import ir.maktab.entitty.enums.PaymentPeriod;
+import ir.maktab.entitty.enums.UniversityType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,6 +17,8 @@ import java.util.Date;
 @EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Loan implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +29,17 @@ public class Loan implements Serializable {
     @CreationTimestamp
     @Temporal(value = TemporalType.TIMESTAMP)
     Date requestDate;
+    @Enumerated(EnumType.STRING)
+    UniversityType universityType;
+
+    public Loan(PaymentPeriod paymentPeriod, Date requestDate, UniversityType universityType) {
+        this.paymentPeriod = paymentPeriod;
+        this.requestDate = requestDate;
+        this.universityType = universityType;
+    }
+
+    public Loan(PaymentPeriod paymentPeriod, Date requestDate) {
+        this.paymentPeriod = paymentPeriod;
+        this.requestDate = requestDate;
+    }
 }
