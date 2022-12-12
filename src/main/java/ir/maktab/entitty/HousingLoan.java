a464c990b373123b23113cf1d0cb951e10e86458
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-12 03:31:29 +0330

    define HousingLoan class

--- src/main/java/ir/maktab/entitty/HousingLoan.java
+++ src/main/java/ir/maktab/entitty/HousingLoan.java
@@ -1,6 +1,7 @@
 package ir.maktab.entitty;
 
 import ir.maktab.entitty.enums.CityType;
+import ir.maktab.entitty.enums.PaymentPeriod;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -8,6 +9,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -15,9 +17,17 @@ import javax.persistence.Enumerated;
 @EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class HousingLoan extends Loan {
     @Enumerated(EnumType.STRING)
     CityType cityType;
     @Embedded
     HouseInfo houseInfo;
+
+    public HousingLoan(PaymentPeriod paymentPeriod, Date requestDate, CityType cityType, HouseInfo houseInfo) {
+        super(paymentPeriod, requestDate);
+        this.cityType = cityType;
+        this.houseInfo = houseInfo;
+    }
 }
