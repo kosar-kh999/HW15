bb93de8ffc93fb6ab2e1ae3a7a845f02843e62bf
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-12 03:34:02 +0330

    define StudentTuitionLoan class

--- src/main/java/ir/maktab/entitty/StudentTuitionLoan.java
+++ src/main/java/ir/maktab/entitty/StudentTuitionLoan.java
@@ -1,12 +1,15 @@
 package ir.maktab.entitty;
 
 import ir.maktab.entitty.enums.Grade;
+import ir.maktab.entitty.enums.PaymentPeriod;
+import ir.maktab.entitty.enums.UniversityType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -14,7 +17,14 @@ import javax.persistence.Enumerated;
 @EqualsAndHashCode
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentTuitionLoan extends Loan {
     @Enumerated(EnumType.STRING)
     Grade grade;
+
+    public StudentTuitionLoan(PaymentPeriod paymentPeriod, Date requestDate, UniversityType universityType, Grade grade) {
+        super(paymentPeriod, requestDate, universityType);
+        this.grade = grade;
+    }
 }
